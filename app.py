d36b7d82d247f266f871a0de801bd9589ccb89cc
manojnehra556-netizen <manojnehra556@gmail.com>
2026-02-05 10:22:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        print(request.form)
     return render_template("index.html")
 
+@app.route("/pricing")
+def pricing():
+    return render_template("pricing.html")
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        print(request.form)
+    return render_template("contact.html")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
